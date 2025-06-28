04a79fd7c20d88d19bbdd22f3cc8aebe64c63565
SH11235 <aabceh1a1a2i3u5o8@gmail.com>
2025-06-29 03:38:46 +0900

    P2P仮実装ファイル削除

--- packages/web/src/pages/P2PTestPage.tsx
+++
@@ -1,9 +0,0 @@
-import { P2PTest } from "../components/P2PTest";
-
-export function P2PTestPage() {
-    return (
-        <div className="min-h-screen bg-gray-100 py-8">
-            <P2PTest />
-        </div>
-    );
-}
